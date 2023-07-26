b755a66e0e940cc33aca402706af1a3589dc03e5
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2023-07-26 18:48:20 -0300

    Finish project

--- src/server.js
+++ src/server.js
@@ -1,26 +1,33 @@
-import http from "node:http"
+// ESModules
+import http, { METHODS } from 'node:http'
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './middlewares/extract-query-params.js'
 
-const users = []
+// CommonJS
+// const http = require('http')
 
-const server = http.createServer((request, response) => {
-    const { method, url } = request
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
 
-    if (method === "GET" && url ==='/users') {
-        return response
-        .setHeader('content-type','application/json')
-        .end(JSON.stringify(users))
-    }
-    if (method === "POST" && url ==='/users') {
-        users.push({
-            id: 1,
-            name: "John Doe",
-            email: "johndoe@example.com"
-        })
+  await json(req, res)
 
-        return response.writeHead(201).end()
-    }
-    
-    return response.writeHead(404).end()
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
+
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
